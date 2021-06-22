6c37499a4e6caab9e2fc93a90765a3be3f53b47b
김종환 <dondon9513@gmail.com>
2021-06-22 19:39:50 +0900

    save 1-3-array.ts

--- 1-TYPE/1-3-array.ts
+++ 1-TYPE/1-3-array.ts
@@ -5,7 +5,15 @@
 
   // readonly 해당 요소를 변경 할 수 없고 읽기만 가능함 (불변성 제공)
   // 단, readonly 사용시 readonly Array<number> 처럼 제네릭은 사용 불가
-  function printArray(fruits: readonly string[]) {
+  function printArray(fruits: readonly string[]) {}
 
-  }
+  // Tuple ( 배열이지만 서로 다른 타입의 데이터를 담을 수 있음 )  <= 사용 권장하지 않음
+  // 이것은 interface, type alias, class 등으로 대체하여 사용
+  let student: [string, number];
+  student = ['name', 123];
+  // 아래처럼 가독성이 떨어짐
+  student[0] // name
+  student[1] // 123
+  // 아래처럼 명시해주는것이 좋음
+  const [name, age] = student;
 }
