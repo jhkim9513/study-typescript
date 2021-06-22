cbd4b79671d90e17df7258a70bbe5661a8531fe8
김종환 <dondon9513@gmail.com>
2021-06-22 17:25:11 +0900

    test commit

---
+++ 1-TYPE/1-1-basic.ts
@@ -0,0 +1,14 @@
+{
+  // JavaScript
+
+  // var : old..  여러가지 문제가 있어 사용하지 않는걸 추천
+  var age = 5;
+  age = 1;
+
+  // let : es6에 도입
+  let name = 'hello';
+  name = 'hi';  //선언된 변수에 새로운 값을 다시 설정 가능
+  // const
+  const age = 5;
+  // age = 7; 한 번 선언된 변수에 새로운 값 설정 불가능
+}
