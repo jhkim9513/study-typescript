9360af93affab9060e86c90935662de8ec65f3af
김종환 <dondon9513@gmail.com>
2021-06-23 16:44:14 +0900

    about abstraction and interface

---
+++ 3-oop/3-4-abstraction.ts
@@ -0,0 +1,107 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
+  //  public : 접근 가능
+  //  private : 어떤곳에서도 직접 접근이 불가능
+  //  protected  : 상속한곳에서는 접근이 가능
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT:number = 7; // class level ( object마다 만들어지지 않기 때문에 memory 낭비를 예방할 수 있다 )
+    private coffeeBeans: number = 0; // instance (object) level
+  
+    private constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans)
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if(beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log('cleaning the machine...');
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if(this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');  
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log('heating up..');
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots} shots...`);
+      return {
+        shots,
+        hasMilk: false,
+      }
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    };
+  }
+
+  /*
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  maker.fillCoffeeBeans(32);
+  maker.makeCoffee(2);
+
+  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
+  // maker2.fillCoffeeBeans(32); <= CoffeeMachine에는 있는 함수지만 CoffeeMaker 인터페이스에는 지정되지 않았기에 사용할 수 없는 상태
+  maker2.makeCoffee(2);
+
+  const maker3: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
+  maker3.fillCoffeeBeans(32);
+  maker3.makeCoffee(2);
+  maker3.clean();
+  */
+
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(45);
+      this.machine.clean();
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  const amateur = new AmateurUser(maker);
+  const pro = new ProBarista(maker);
+  amateur.makeCoffee();
+  pro.makeCoffee();
+}
